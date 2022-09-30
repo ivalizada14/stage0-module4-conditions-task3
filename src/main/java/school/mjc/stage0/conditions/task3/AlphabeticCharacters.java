8cfd5cdccc3d8afbc9122ff26a0f3ead6c5130a5
Ibrahim Velizade <ivalizada14@gmail.com>
2022-09-30 19:17:27 +0300

    first try

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.conditions.task3;
 
 public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
+        if(character=='a' || character=='A' ||
+                character=='e' || character=='E' ||
+                character=='i' || character=='I' ||
+                character=='o' || character=='O' ||
+                character=='u' || character=='U' )
+            System.out.println("Vowel");
+        else if(character<'A'||character>'z')
+            System.out.println("wrong alphabet!");
+        else
+            System.out.println("Consonant");
     }
 }
